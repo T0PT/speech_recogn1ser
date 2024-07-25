41791019e8951db82c39324ed6732d83a4ac8ebe
Alesha314 <alex.baltr@gmail.com>
2024-07-25 18:46:37 +0200

    fixed setup

--- setup.bat
+++ setup.bat
@@ -1,6 +1,7 @@
 pip install tk
 pip install -U openai-whisper
 pip install setuptools-rust
-pip install torch==2.3.1
+@REM pip install torch==2.3.1
+@REM pip install numpy==1.26.4
 @REM pip install shutil
 choco install ffmpeg
